ebf6b6c8d6908516f5e1439ebbba973f279b6a6d
Rupam Bose <rupambose20@gmail.com>
2025-12-10 01:12:07 +0600

    Login and SignUp design and java file completed

--- app/src/main/java/com/rupambose/knowledgeflow/register/loginsignupactivity.java
+++ app/src/main/java/com/rupambose/knowledgeflow/register/loginsignupactivity.java
@@ -1,9 +1,14 @@
 package com.rupambose.knowledgeflow.register;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,11 +17,60 @@ import com.rupambose.knowledgeflow.R;
 
 public class loginsignupactivity extends AppCompatActivity {
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loginsignupactivity);
+
+        EditText login_Name = findViewById(R.id.emailEditText);
+        EditText login_Pass = findViewById(R.id.passwardEditText);
+        AppCompatButton login = findViewById(R.id.login_btn);
+        EditText signUp_email = findViewById(R.id.SignUp_Email);
+        EditText signUp_pass = findViewById(R.id.SignUp_Password);
+        EditText signUp_name = findViewById(R.id.SignUp_Name);
+        TextView txt = findViewById(R.id.textView4);
+        AppCompatButton signUp = findViewById(R.id.signup_btn);
+
+        String action = getIntent().getStringExtra("Action");
+        if("logIn".equals(action)){
+            login_Name.setVisibility(View.VISIBLE);
+            login_Pass.setVisibility(View.VISIBLE);
+            login.setVisibility(View.VISIBLE);
+            txt.setVisibility(View.VISIBLE);
+            signUp.setVisibility(View.VISIBLE);
+            signUp_email.setVisibility(View.GONE);
+            signUp_name.setVisibility(View.GONE);
+            signUp_pass.setVisibility(View.GONE);
+        }
+        else if("signUp".equals(action)){
+            login_Name.setVisibility(View.GONE);
+            login_Pass.setVisibility(View.GONE);
+            login.setVisibility(View.GONE);
+            txt.setVisibility(View.GONE);
+            signUp.setVisibility(View.VISIBLE);
+            signUp_email.setVisibility(View.VISIBLE);
+            signUp_name.setVisibility(View.VISIBLE);
+            signUp_pass.setVisibility(View.VISIBLE);
+
+        }
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login_Name.setVisibility(View.GONE);
+                login_Pass.setVisibility(View.GONE);
+                login.setVisibility(View.GONE);
+                txt.setVisibility(View.GONE);
+                signUp.setVisibility(View.VISIBLE);
+                signUp_email.setVisibility(View.VISIBLE);
+                signUp_name.setVisibility(View.VISIBLE);
+                signUp_pass.setVisibility(View.VISIBLE);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/rupambose/knowledgeflow/register/welcome.java
+++ app/src/main/java/com/rupambose/knowledgeflow/register/welcome.java
@@ -1,9 +1,12 @@
 package com.rupambose.knowledgeflow.register;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,11 +15,34 @@ import com.rupambose.knowledgeflow.R;
 
 public class welcome extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
+        AppCompatButton login_btn = findViewById(R.id.login_btn);
+        AppCompatButton signUp_btn = findViewById(R.id.signup_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(welcome.this, loginsignupactivity.class);
+                intent.putExtra("Action","logIn");
+                startActivity(intent);
+            }
+        });
+
+        signUp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(welcome.this, loginsignupactivity.class);
+                intent.putExtra("Action","signUp");
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
