e0817e56b462c49348abb8da47d28bdcea3861e1
Rupam Bose <rupambose20@gmail.com>
2025-12-26 20:29:49 +0600

    blogitem is added

---
+++ app/src/main/java/com/rupambose/knowledgeflow/Home/adapter/blogItem.java
@@ -0,0 +1,11 @@
+package com.rupambose.knowledgeflow.Home.adapter;
+
+public class blogItem {
+    String contentTitle;
+    String profilePic;
+    String profileName;
+    String date;
+    String post;
+    int likesCount;
+    int commentsCount;
+}
